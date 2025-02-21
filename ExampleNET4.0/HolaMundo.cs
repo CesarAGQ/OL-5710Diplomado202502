36177931b9226c0b4437e41a9288c24f411fd75c
Cesar Gomez <ca_gomez87@hotmail.com>
2025-02-20 19:46:42 -0600

    first commit

---
+++ ExampleNET4.0/HolaMundo.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string nombrePersona = "";
+		
+		Console.Write("Escribe tu nombre: ");
+		nombrePersona = Console.ReadLine();
+		Console.WriteLine("Tu nombre es: " + nombrePersona);
+	}
+}
